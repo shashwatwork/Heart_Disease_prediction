c35f8806df635b39215b8c1aefd270a5676a61c4
SHASHWAT TIWARI <stiwari2719@gmail.com>
2019-12-15 21:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def home():
     return "Welcome to Heart Prediction API!"
 class Predictheart(Resource):
     def get(self, vizdata):
+        print(vizdata)
         info = vizdata.split(",")
         age = int(info[0])
         sex = int(info[1])
